66b03e8187ac11efc83fc3b98d3d94087fc9ffda
stefnotch <brandmairstefan@gmail.com>
2021-01-06 20:59:20 +0100

    Add <Formulae>meth</Formulae> component

---
+++ src/.vuepress/components/Formulae.vue
@@ -0,0 +1,17 @@
+<script functional>
+const katex = require("katex");
+
+export default {
+  functional: true,
+  render(createElement, context) {
+    const defaultSlot = context.slots().default;
+    return createElement("div", {
+      domProps: {
+        innerHTML: katex.renderToString(
+          defaultSlot.map((v) => v.text).join("")
+        ),
+      },
+    });
+  },
+};
+</script>
