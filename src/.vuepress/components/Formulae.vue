cfa433d062e1eb3b64053d789268f081998eb52b
stefnotch <brandmairstefan@gmail.com>
2021-01-07 18:15:19 +0100

    Add class to formulae

--- src/.vuepress/components/Formulae.vue
+++ src/.vuepress/components/Formulae.vue
@@ -6,6 +6,7 @@ export default {
   render(createElement, context) {
     const defaultSlot = context.slots().default;
     return createElement("div", {
+      staticClass: "formulae",
       domProps: {
         innerHTML: katex.renderToString(
           defaultSlot.map((v) => v.text).join("")
